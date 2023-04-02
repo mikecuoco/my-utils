b8f9f5269ef570aa4a2d689d6cf7f1cbc5b5ebb9
mikecuoco <mcuoco@ucsd.edu>
2023-04-01 17:52:29 -0700

    format and isort

--- tests/test_read_rmsk.py
+++ tests/test_read_rmsk.py
@@ -1,14 +1,17 @@
 import pytest
+
 from my_utils.rmsk import read_rmsk
 
+
 def test_read_rmsk():
-	df = read_rmsk("tests/rmsk.out")
-	assert df.shape[1] == 18, "Wrong number of columns"
-	assert "repClass" in df.columns, "repClass column not found"
-	assert "repFamily" in df.columns, "repFamily column not found"
-	assert "length" in df.columns, "length column not found"
-	assert "age" in df.columns, "age column not found"
+    df = read_rmsk("tests/rmsk.out")
+    assert df.shape[1] == 18, "Wrong number of columns"
+    assert "repClass" in df.columns, "repClass column not found"
+    assert "repFamily" in df.columns, "repFamily column not found"
+    assert "length" in df.columns, "length column not found"
+    assert "age" in df.columns, "age column not found"
+
 
 def test_ucsc_rmsk():
-	with pytest.raises(AssertionError) as e:
-		df = read_rmsk("tests/rmsk.ucsc.txt")
+    with pytest.raises(AssertionError) as e:
+        df = read_rmsk("tests/rmsk.ucsc.txt")
